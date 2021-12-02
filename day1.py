3433339e9edb882b5d4f84c9aef3f80ae190fb5b
brwang <brwang@linkedin.com>
2021-12-02 15:27:46 -0800

    day 1

---
+++ day1.py
@@ -0,0 +1,15 @@
+
+file = open('day1/input', 'r')
+count = 0
+pre = file.readline()
+while pre:
+    line = file.readline()
+ 
+    if not line:
+        break
+    
+    if line > pre:
+        count += 1
+
+print(f"Part 1: {count}")
+file1.close()
