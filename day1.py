ae6e5602c5485dbcf1e582285e9860f9782d696b
brwang <brwang@linkedin.com>
2021-12-02 17:09:51 -0800

    format

--- day1.py
+++ day1.py
@@ -1,16 +1,16 @@
-file = open('day/1/input', 'r')
-count = 0
-pre = file.readline()
-while pre:
-    line = file.readline()
- 
-    if not line:
-        break
-    
-    if line > pre:
-        count += 1
+with open('day1/input') as file:
+    count = 0
+    pre = file.readline()
+    while pre:
+        line = file.readline()
+     
+        if not line:
+            break
+        
+        if line > pre:
+            count += 1
 
-    pre = line
+        pre = line
 
-print(count)
-file.close()
+    print(count)
+    file.close()
