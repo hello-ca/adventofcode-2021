864b29e8f31d1126319cf07c28b8a4aac0435230
brwang <brwang@linkedin.com>
2021-12-04 21:48:50 -0800

    Part 1 for day4

---
+++ day4.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+DEBUG = False
+
+def log(str):
+    if DEBUG:
+        print(str)
+
+#part 1
+GRIDSIZE = 5
+def getGrids(file):
+    total = 0
+    grids = []
+    while True:
+        line = file.readline()
+     
+        if not line:
+            break
+
+        if line == '\n':
+            continue
+
+        if (total % GRIDSIZE == 0):
+            grid = [[]*GRIDSIZE]*GRIDSIZE
+
+        grid[total % GRIDSIZE] = list(map(int, line.split()))
+        total += 1
+
+        if (total % GRIDSIZE == 0):
+            grids.append(grid)
+    return grids
+
+
+def checkGrids(grids, numbers):
+    result2 = [[-1]*2] * len(grids)
+    all_index = len(numbers) + 1
+    score = 0
+    for i in range(len(grids)):
+        result2[i] = checkGrid(grids[i], numbers)
+        if result2[i][0] < all_index:
+            all_index = result2[i][0]
+            score = result2[i][1]
+
+    log(result2)
+    print(score)
+    print(all_index)
+
+def checkGrid(grid, numbers):
+    newgrid = np.array(grid)
+
+    tmp = np.array(grid)
+    log(numbers)
+    found = -1
+    
+    for j in range(0, len(numbers)):
+        tmp[tmp == numbers[j]] = -1
+        for i in range(GRIDSIZE):
+            # print(numbers[j])
+            if np.all(tmp[i] ==  -1):
+                log('found row:' + str(j))
+                found = j
+                break;
+
+            if np.all(tmp[:, i] ==  -1):
+                log('found col:' + str(j))
+                found = j
+                break;
+
+        if found > -1:
+            break
+
+
+    # print(tmp)
+    tmp[tmp == -1] = 0  
+    log('-------' + str(numbers[found]) + ',sum:' + str(tmp.sum()))
+    # print(tmp)
+    return [found, numbers[found] * tmp.sum()]
+
+with open('day4/input') as file:
+    line = file.readline().replace('\n', '')
+    numbers = []
+    log(line)
+    items = line.split(',')
+    # numbers = np.empty([len(items)])
+    for item in items:
+        numbers.append(int(item))
+
+    log(numbers)
+    np_numbers = np.array(numbers);
+    log(np_numbers)
+
+    grids = getGrids(file)
+    checkGrids(grids, np_numbers)
+
+    
+    log(grids)
+    log(grids[0][0][1])
+    file.close()
+
+
+#part 2
