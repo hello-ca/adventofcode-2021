a22b92315b0e5a50fd8fb945abfc18a394b1f26f
brwang <brwang@linkedin.com>
2021-12-04 23:51:33 -0800

    day5 part 2

--- day5.py
+++ day5.py
@@ -36,9 +36,28 @@ def getResult(items):
         if a[0] == b[0]:
             for y in range(min(a[1], b[1]), max(a[1], b[1]) + 1):
                 arr[a[0]][y] += 1
-        else:
+        elif a[1] == b[1]:
             for x in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
                 arr[x][a[1]] += 1
+        else:
+            print('diagonal')
+            print(grids[i])
+            flag = 1
+            if a[0] < b[0]:
+                if a[1] > b[1]:
+                    flag = -1
+                offset = 0
+                for x in range(a[0], b[0] + 1):
+                    arr[x][a[1] + offset] += 1
+                    offset += flag
+            else:
+                if b[1] > a[1]:
+                    flag = -1
+                offset = 0
+                for x in range(b[0], a[0] + 1):
+                    arr[x][b[1] + offset] += 1
+                    offset += flag
+
 
     print(arr)
     print(np.count_nonzero(arr > 1))
@@ -53,9 +72,18 @@ with open('day5/input') as file:
 
         vent = getPos(line)
 
-        if vent[0][0] == vent[1][0] or vent[0][1] == vent[1][1]:
-            grids.append(getPos(line))
+        if vent[0][0] == vent[1][0] or vent[0][1] == vent[1][1]:   
+            grids.append(vent)
 
+        # for part 2
+        if vent[0][0] != vent[1][0] and vent[0][1] != vent[1][1]:
+            if abs(vent[0][0] - vent[1][0]) == abs(vent[0][1] - vent[1][1]):
+                grids.append(vent)
+
+
+    print(grids)
+    print('-------')
     getResult(grids)
+
     file.close()
 
